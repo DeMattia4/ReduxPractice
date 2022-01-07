dbccd39279061493dc0579a45e42a3ffa61a4f74
Dave DeMattia <davedemattia@quadax.com>
2022-01-07 11:00:01 -0500

    first commit

---
+++ src/store/auth.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Authentication State
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export default authSlice.reducer;
+export const authActions = authSlice.actions;
